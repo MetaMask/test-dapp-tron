1e5a1c498fb042707e115baba6f8f83fee23c295
baptiste-marchand <baptiste-marchand@users.noreply.github.com>
2026-01-07 14:25:03 +0000

    deploy: 97e7525bbc3ded0ada6f6e96f4ae8117173faeaf

---
+++ 0.2.2/test.d.ts
@@ -0,0 +1,44 @@
+type Pathify<T, Prefix extends string = ''> = {
+    [K in keyof T]: T[K] extends true ? `${Prefix extends '' ? '' : `${Prefix}.`}${Extract<K, string>}` : T[K] extends object ? Pathify<T[K], `${Prefix extends '' ? '' : `${Prefix}.`}${Extract<K, string>}`> : never;
+};
+/**
+ * The list of test ids to access elements in the e2e2 tests.
+ */
+export declare const dataTestIds: Pathify<{
+    readonly testPage: {
+        readonly header: {
+            readonly id: true;
+            readonly endpoint: true;
+            readonly updateEndpoint: true;
+            readonly connect: true;
+            readonly disconnect: true;
+            readonly account: true;
+            readonly connectionStatus: true;
+        };
+        readonly signMessage: {
+            readonly id: true;
+            readonly message: true;
+            readonly signMessage: true;
+            readonly signedMessage: true;
+        };
+        readonly sendUSDT: {
+            readonly id: true;
+            readonly address: true;
+            readonly signTransaction: true;
+            readonly sendTransaction: true;
+            readonly signedTransaction: true;
+            readonly transactionHash: true;
+            readonly amount: true;
+        };
+        readonly sendTRX: {
+            readonly id: true;
+            readonly address: true;
+            readonly signTransaction: true;
+            readonly sendTransaction: true;
+            readonly signedTransaction: true;
+            readonly transactionHash: true;
+            readonly amount: true;
+        };
+    };
+}, "">;
+export {};
